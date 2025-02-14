da979f2fe6c9eb21cc82307ce5737b2af76a9a82
Manshi <kumarimanshi814@gmail.com>
2025-02-15 00:33:12 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+// import logo from '';
+import './App.css';
+import Dashboard from './Dashboard';
+
+function App() {
+  return (
+   <Dashboard/>
+  );
+}
+
+export default App;
